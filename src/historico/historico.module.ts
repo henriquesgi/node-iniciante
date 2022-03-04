737e101e3811f2ce173f669c570ccf5bedc7101e
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-03-03 23:28:10 -0300

    refactor: typeORM inserido no módulo historico

--- src/historico/historico.module.ts
+++ src/historico/historico.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { HistoricoService } from './historico.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Historico } from './entities/historico.entity';
 import { HistoricoController } from './historico.controller';
+import { HistoricoService } from './historico.service';
 
 @Module({
   controllers: [HistoricoController],
+  imports: [TypeOrmModule.forFeature([Historico])],
   providers: [HistoricoService]
 })
-export class HistoricoModule {}
+export class HistoricoModule { }
