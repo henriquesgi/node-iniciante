12efd32c305cccad7f4eb4049e3858d4ba46846d
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-03-16 23:16:47 -0300

    refactor: pipe de validação de cnh

--- src/common/pipes/cnh-validation.pipe.ts
+++ src/common/pipes/cnh-validation.pipe.ts
@@ -6,7 +6,7 @@ export class CnhValidationPipe implements PipeTransform {
   transform(value: number, metadata: ArgumentMetadata) {
 
     if (isNaN(value) || value < 0 || value > 1000) {
-      throw new BadRequestException('Requisição inválida')
+      throw new BadRequestException()
     }
 
     return value;
