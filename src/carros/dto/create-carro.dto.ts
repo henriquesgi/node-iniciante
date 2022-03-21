91fc6f71d4a30f5625f73006f9d3dfba54b2c35a
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-03-20 22:34:08 -0300

    feat: descrição (Swagger) para o DTO carros criada

--- src/carros/dto/create-carro.dto.ts
+++ src/carros/dto/create-carro.dto.ts
@@ -1,28 +1,46 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsNumber,
   IsPositive,
   IsString,
-  Length,
   Max,
-  MaxLength
+  MaxLength,
+  MinLength
 } from "class-validator";
 
 export class CreateCarroDto {
   @IsString()
-  @Length(7)
+  @MaxLength(7)
+  @MinLength(7)
+  @ApiProperty({
+    description: 'Identificação única do carro.',
+    type: String
+  })
   placa: string;
 
   @IsNumber()
   @IsPositive()
   @Max((new Date()).getUTCFullYear())
+  @ApiProperty({
+    description: 'Ano de fabricação do carro.',
+    type: Number
+  })
   ano: number;
 
   @IsString()
   @MaxLength(50)
+  @ApiProperty({
+    description: 'Modelo do carro.',
+    type: String
+  })
   modelo: string;
 
   @IsNumber()
   @IsPositive()
   @Max(5)
+  @ApiProperty({
+    description: 'Quantidade de passageiros do carro.',
+    type: Number
+  })
   passageiros: number;
 }
