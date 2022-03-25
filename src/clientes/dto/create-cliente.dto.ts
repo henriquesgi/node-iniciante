5f0cb34d27b422311f4af2c7b2eb25202e01bf56
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-03-24 22:30:38 -0300

    feat: descrição (Swagger) do DTO clientes criada

--- src/clientes/dto/create-cliente.dto.ts
+++ src/clientes/dto/create-cliente.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsNumber,
   IsPositive,
@@ -10,13 +11,25 @@ export class CreateClienteDto {
   @IsNumber()
   @IsPositive()
   @Max(1000)
+  @ApiProperty({
+    description: 'Identificação única do cliente.',
+    type: Number
+  })
   cnh: number;
 
   @IsString()
   @MaxLength(50)
+  @ApiProperty({
+    description: 'Endereço do cliente.',
+    type: String
+  })
   endereco: string;
 
   @IsString()
   @MaxLength(50)
+  @ApiProperty({
+    description: 'Nome do cliente.',
+    type: String
+  })
   nome: string;
 }
