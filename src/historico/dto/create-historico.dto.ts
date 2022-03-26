2369a7aaddc9d3ef747271cc71f23e6882619abe
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-03-25 23:37:06 -0300

    feat: descrição (Swagger) do DTO historico criada

--- src/historico/dto/create-historico.dto.ts
+++ src/historico/dto/create-historico.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsDateString,
   IsNumber,
@@ -11,16 +12,32 @@ export class CreateHistoricoDto {
   @IsNumber()
   @IsPositive()
   @Max(1000)
-  cnh: string;
+  @ApiProperty({
+    description: 'Identificação única do cliente.',
+    type: Number
+  })
+  cnh: number;
 
   @IsString()
   @Length(7)
+  @ApiProperty({
+    description: 'Identificação única do carro.',
+    type: String
+  })
   placa: string;
 
   @IsDateString()
+  @ApiProperty({
+    description: 'Data de devolução prevista para o cliente entregar o carro.',
+    type: Date
+  })
   dataDevolucaoPrevista: Date;
 
   @IsNumber()
   @IsPositive()
+  @ApiProperty({
+    description: 'Valor do aluguel de um carro a ser cobrado do cliente.',
+    type: Number
+  })
   valorAluguel: number;
 }
