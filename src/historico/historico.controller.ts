fda75db05d702406f232e703a3dfc340cf0265fc
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-03-25 23:34:29 -0300

    feat: Swagger implementado no controller historico

--- src/historico/historico.controller.ts
+++ src/historico/historico.controller.ts
@@ -1,34 +1,60 @@
 import {
   Body,
+  ConflictException,
   Controller,
   Get,
+  NotFoundException,
   Param,
   ParseIntPipe,
   Patch,
   Post,
+  UnprocessableEntityException,
   ValidationPipe
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CreateHistoricoDto } from './dto/create-historico.dto';
 import { HistoricoService } from './historico.service';
 
 
 @Controller('historico')
+@ApiBearerAuth()
+@ApiTags('Histórico')
 export class HistoricoController {
   constructor(private readonly historicoService: HistoricoService) { }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Array contendo todas as representações.' })
   findAll() {
     return this.historicoService.findAll();
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number) {
-    return this.historicoService.update(id);
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Representação modificada.' })
+  @ApiResponse({ status: 400, description: 'Representação inválida.' })
+  @ApiResponse({ status: 404, description: 'Representação não encontrada.' })
+  async update(@Param('id', ParseIntPipe) id: number) {
+    const request = await this.historicoService.update(id);
+    if (request === 0) {
+      throw new NotFoundException()
+    }
   }
 
   @Post()
-  create(@Body(ValidationPipe) historico: CreateHistoricoDto) {
-    return this.historicoService.create(historico);
+  @ApiBody({ type: CreateHistoricoDto })
+  @ApiResponse({ status: 201, description: 'Representação criada.' })
+  @ApiResponse({ status: 400, description: 'Representação inválida.' })
+  @ApiResponse({ status: 422, description: 'Representação com valores inválidos.' })
+  async create(@Body(ValidationPipe) historico: CreateHistoricoDto) {
+    const request = await this.historicoService.create(historico);
+    switch (request) {
+      case '22003':
+        throw new UnprocessableEntityException('Precisão fora do limite p(6,2).')
+      case '23503':
+        throw new UnprocessableEntityException('Violação de chave estrangeira.')
+      default:
+        break;
+    }
   }
 }
