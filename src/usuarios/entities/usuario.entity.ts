d4beb412b21b342fb5690123dec1be88a2809307
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-04-10 23:33:57 -0300

    refactor: entidade usuario modificada

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -3,11 +3,11 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 @Entity()
 export class Usuario {
   @PrimaryColumn()
-  username: string
+  id: string
 
   @Column()
   nome: string
 
   @Column()
-  password: string
+  senha: string
 }
