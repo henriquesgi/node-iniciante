cbae225626685de3797c39d93a79e6fe4e926867
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-04-17 20:13:50 -0300

    feat: funcionalidades do projeto implementadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { CarrosModule } from './carros/carros.module';
+import { ClientesModule } from './clientes/clientes.module';
+import { HistoricoModule } from './historico/historico.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    CarrosModule,
+    ClientesModule,
+    HistoricoModule,
+    UsuariosModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+      migrations: ['migrations/*js'],
+      cli: {
+        migrationsDir: './src/db/migration'
+      }
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    }
+  ]
 })
-export class AppModule {}
+export class AppModule { }
