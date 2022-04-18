469b653bcb65b5df0b242baee4feec06d8567c01
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-04-17 22:34:43 -0300

    feat: controller da aplicação criado

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards
+} from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+
+import { AuthService } from './auth/auth.service';
+import { SkipJwt } from './auth/constants';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
+@ApiTags('/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('auth/login')
+  @SkipJwt()
+  @UseGuards(LocalAuthGuard)
+  @ApiBody({ schema: { type: 'object', properties: { id: { type: 'string' }, senha: { type: 'string' } } } })
+  @ApiResponse({ status: 200, description: 'Login bem sucedido.' })
+  @ApiResponse({ status: 401, description: 'Login sem sucesso.' })
+  async login(@Req() req: Request, @Res() res: Response) {
+    const token = await this.authService.login(req.user);
+    if (token) {
+      res.status(HttpStatus.OK).json(token);
+    }
   }
 }
