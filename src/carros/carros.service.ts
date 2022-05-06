97d3cdc882822fbc8ff9cf2457eeb1122fb18897
Henrique <45628939+henriquesgi@users.noreply.github.com>
2022-05-05 23:04:51 -0300

    docs: documentação do serviço carros sendo criada

--- src/carros/carros.service.ts
+++ src/carros/carros.service.ts
@@ -12,6 +12,14 @@ export class CarrosService {
     private carroRepository: Repository<Carro>,
   ) { }
 
+  /**
+   * Cria um registro na tabela {@link Carro}
+   * 
+   * @param carro
+   * 
+   * @returns Void quando o usuário é criado.
+   * @returns Um código em string quando algum erro acontecer durante o insert.
+   */
   async create(carro: CreateCarroDto): Promise<void | string> {
     try {
       await this.carroRepository.insert({ ...carro });
@@ -20,14 +28,38 @@ export class CarrosService {
     }
   }
 
-  findAll(pagina: number = 0): Promise<Carro[]> {
+  /**
+   * Busca todos os registros na tabela {@link Carro}.
+   * 
+   * A paginação garante que sejam retornados até 25 registros por página.
+   * 
+   * @param pagina
+   * 
+   * @returns Array contendo de 0 até 25 registros.
+   */
+  findAll(pagina: number): Promise<Carro[]> {
     return this.carroRepository.find({ take: 25, skip: pagina * 25 });
   }
 
+  /**
+   * Busca 1 registro na tabela {@link Carro}.
+   * 
+   * @param placa Identificação do registro, PK.
+   * 
+   * @returns Caso encontrado, 1 registro.
+   */
   findOne(placa: string): Promise<Carro> {
     return this.carroRepository.findOne(placa);
   }
 
+  /**
+   * Remove, por exclusão lógica, 1 registro na tabela {@link Carro}.
+   * 
+   * @param placa Identificação do registro, PK.
+   * 
+   * @returns O número 1 caso algum registro for excluído logicamente.
+   * @returns O número 0 caso nenhum registro for excluído logicamente.
+   */
   async remove(placa: string): Promise<number> {
     const query = await this.carroRepository.update(
       { placa, excluido: false },
@@ -36,6 +68,15 @@ export class CarrosService {
     return query.affected
   }
 
+  /**
+   * Modifica 1 registro na tabela {@link Carro}.
+   * 
+   * @param placa Identificação do registro, PK.
+   * @param alugado Flag para modificar o registro.
+   * 
+   * @returns O número 1 caso algum registro for modificado.
+   * @returns O número 0 caso nenhum registro for modificado.
+   */
   async update(placa: string, alugado: boolean): Promise<number> {
     const query = await this.carroRepository.update(placa, { alugado });
     return query.affected
